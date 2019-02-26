9944e3729fd3ae97d51f4cf63e62e2c3c8d833ca
Osman <okali@uci.edu>
2019-02-25 21:54:24 -0800

    Make all kindzz of upgrades

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import phaser from 'phaser';
 import Game from './scenes/Game';
 
 const config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    parent: 'content',
-    scene: [
-        Game
-    ]
+	type: Phaser.AUTO,
+	width: window.innerWidth,
+	height: window.innerHeight,
+	parent: 'content',
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: true
+		}
+	},
+	scene: [
+			Game
+	]
 };
 
 const game = new phaser.Game(config);
